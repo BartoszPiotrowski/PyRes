c02623f97871aaa9a12dced1af22d92afbd88b4f
Bartosz Piotrowski <bartoszpiotrowski@post.pl>
2020-05-01 21:02:28 +0200

    Simple neural policy model.

--- policy_network.py
+++ policy_network.py
@@ -2,33 +2,48 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 import torch.nn.functional as F
+import numpy as np
 
 
-class PolicyNetwork(nn.Module):
+class PolicyNetwork:
     def __init__(self, num_features=None, num_actions=None,
                  num_hidden_layers=0, num_units=64, learning_rate=0.001):
-        super(Net, self).__init__()
-        self.num_hidden_layers = num_hidden_layers
-        self.num_uits = num_units
+        layers = []
+        layers.append(nn.Linear(num_features, num_units))
+        layers.append(nn.ReLU())
+        for _ in range(num_hidden_layers):
+            layers.append(nn.Linear(num_units, num_units))
+            layers.append(nn.ReLU())
+        layers.append(nn.Linear(num_units, num_actions))
+        layers.append(nn.Softmax(dim=1))
+        self.model = nn.Sequential(*layers)
         self.learning_rate = learning_rate
-        self.layers = []
-        self.layers.append(nn.Linear(num_features, num_units))
-        for _ in range(self.num_hidden_layers)
-            self.layers.append(nn.Linear(self.num_units, self.num_units))
-        self.layers.append(nn.Linear(self.num_units, self.num_actions))
+        self.loss = nn.MSELoss()
+        self.optimizer = optim.SGD(self.model.parameters(),
+                                   lr=self.learning_rate)
 
-        self.optimizer = optim.SGD(self.parameters(), lr=self.learning_rate)
-
-    def forward(self, x):
-        for layer in self.layers:
-            x = F.relu(layer(x))
-        x = F.softmax(x)
-        return x
-
-    def train(self, batch):
-        output = self.forward(...)
-        target = ...
-        loss = nn.MSELoss()(output, target)
-        loss.backward()
+    def train(self, batch_states, batch_actions, batch_returns):
+        batch_states = torch.Tensor(batch_states)
+        batch_actions = torch.Tensor(batch_actions)
+        batch_returns = torch.Tensor(batch_returns)
+        predicted_actions = self.model(batch_states)
+        # TODO use returns, add logarithm
+        self.loss(predicted_actions, batch_actions)
+        self.loss.backward()
         self.optimizer.zero_grad()
         self.optimizer.step()
+
+    def predict(self, batch_states):
+        batch_states = torch.Tensor(batch_states)
+        return self.model(batch_states)
+
+
+if __name__=='__main__':
+    policy_network = PolicyNetwork(4, 3, 1, 64, 0.01)
+    batch_states = [[1,2,3,4],[5,6,7,8]]
+    batch_states = torch.Tensor(batch_states)
+    print(policy_network.model)
+    #print(list(policy_network.model.parameters()))
+    actions = policy_network.predict(batch_states)
+    print(actions)
+
