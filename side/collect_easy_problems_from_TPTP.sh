ceb8606d81c644d0138044e885c1cc1e8c5632ec
Bartosz Piotrowski <bartoszpiotrowski@post.pl>
2020-05-21 14:54:46 +0200

    collect easy TPTP problems

---
+++ side/collect_easy_problems_from_TPTP.sh
@@ -0,0 +1,11 @@
+TPTP_DIR=$1
+EASY_DIR=$2
+ALL_PROBLEMS=`find $TPTP_DIR/Problems -type f -name '*+*.p'`
+for f in $ALL_PROBLEMS
+do
+	grep 'Rating   : 0.0[01]' $f -l >> _easy
+done
+
+mkdir -p $EASY_DIR
+cat _easy | xargs cp -t $EASY_DIR
+
